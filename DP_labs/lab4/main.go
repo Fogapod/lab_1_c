785e41cbfd38436f78c11ef4bb2a3f0a30e6e922
Fogapod <gish.ee18@gmail.com>
2018-09-13 16:50:05 +0300

    add lab4

---
+++ DP_labs/lab4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	emailRegex := regexp.MustCompile(`[\w.\-]+@([\w\d]+\.)+[\w\d]{2,}`)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Enter email: ")
+		userInput, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input")
+			continue
+		}
+
+		if emailRegex.MatchString(userInput[:len(userInput)-1]) {
+			fmt.Println("Correct format")
+		} else {
+			fmt.Println("Wrong format")
+		}
+	}
+}
