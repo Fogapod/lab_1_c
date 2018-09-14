ff6782aade7b154f2f926aa9f4277d8d3d53ca37
Eugene <gish.ee18@gmail.com>
2018-09-14 10:30:35 +0300

    add lab7_v11

---
+++ DP_labs/lab6_v11/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	arrayLen = 5
+)
+
+func getInt() int {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Enter int: ")
+		value, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading stdin: ", err)
+			continue
+		}
+
+		converted, err := strconv.Atoi(value[:len(value)-1])
+		if err != nil {
+			fmt.Println("Error converting int: ", err)
+			continue
+		}
+
+		return converted
+	}
+}
+
+type Array struct {
+	array []int
+}
+
+func NewArray() Array {
+	return Array{array: make([]int, arrayLen)}
+}
+
+func (arr *Array) FIll() {
+	for i := 0; i < arrayLen; i++ {
+		arr.array[i] = getInt()
+	}
+}
+
+func (arr Array) Print() {
+	fmt.Println(arr.array)
+}
+
+func (arr Array) findFIrstMaxPos() int {
+	maxItem := arr.array[len(arr.array)-1]
+	maxItemPos := len(arr.array) - 1
+
+	for i := len(arr.array) - 1; i > 0; i-- {
+		if arr.array[i] >= maxItem {
+			maxItem = arr.array[i]
+			maxItemPos = i
+		}
+	}
+
+	return maxItemPos
+}
+
+func (arr *Array) prependBeforeZeros(n int) {
+	newArr := []int{}
+	for i := 0; i < len(arr.array); i++ {
+		if arr.array[i] == 0 {
+			newArr = append(newArr, n)
+		}
+		newArr = append(newArr, arr.array[i])
+	}
+
+	arr.array = newArr
+}
+
+func main() {
+	arr := NewArray()
+	arr.FIll()
+	arr.Print()
+	fmt.Println("Position of first max item:", arr.findFIrstMaxPos())
+	fmt.Println("Enter value to prepend before zeros:")
+	value := getInt()
+	arr.prependBeforeZeros(value)
+	arr.Print()
+}
