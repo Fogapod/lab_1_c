b838a6ea5da0bf3843354e90a990fa1f6f92e0a7
Fogapod <gish.ee18@gmail.com>
2018-09-15 12:15:10 +0300

    add lab10_v11

---
+++ DP_labs/lab10_v11/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func reversePrint(arr []int, index int) {
+	if index < len(arr)-1 {
+		reversePrint(arr, index+1)
+	}
+
+	fmt.Printf("%d ", arr[index])
+}
+
+func main() {
+	reversePrint([]int{1, 2, 3, 4, 5}, 0)
+}
